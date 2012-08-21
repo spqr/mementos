a3e0c9b08e4e11701a41c506393742b59f7f3e44
Ben Ransford <ransford@cs.umass.edu>
2012-08-21 12:28:55 -0400

    proper alignment for main()

--- samples/add.c
+++ samples/add.c
@@ -1,4 +1,9 @@
-#include "common.h"
+// #include "common.h"
 
 int add(int a, int b) { return a+b; }
+#ifdef __clang__
+// __attribute__((section(".fini9"))) void donothing () { }
+__attribute__((section(".init9")))
+__attribute__((aligned(2)))
+#endif // __clang __
 int main (void) { return add(5, 6); }
