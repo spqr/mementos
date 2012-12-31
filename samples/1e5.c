552e6eaad8464336577d72d070feb94c6db54e03
Ben Ransford <ransford@cs.umass.edu>
2012-12-31 16:22:18 -0500

    nice clean 1e5 test case (just a long loop)

--- samples/1e5.c
+++ samples/1e5.c
@@ -1,14 +1,20 @@
 #include "common.h"
+#include <msp430.h>
+
+unsigned int x;
 
 void foo (void) {
     register unsigned int i;
-    for (i = 0; i < 65500; ++i) {
-        asm("NOP");
+    for (i = 0; i < 10000; ++i) {
+        asm volatile("NOP");
     }
 }
 
 MEMENTOS_MAIN_ATTRIBUTES
 int main (void) {
-    foo();
-    return 0;
+    WDTCTL = WDTPW + WDTHOLD; // stop WDT
+    for (x = 0; x < 10; ++x) {
+        foo();
+    }
+    return 99;
 }
