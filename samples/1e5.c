3102bed69935c82e4e490873ee81a6ad625205ef
Ben Ransford <ransford@cs.umass.edu>
2013-01-16 10:53:24 -0500

    make foo() return a value in 1e5 sample

--- samples/1e5.c
+++ samples/1e5.c
@@ -3,18 +3,19 @@
 
 unsigned int x;
 
-void foo (void) {
+int foo (void) {
     register unsigned int i;
     for (i = 0; i < 10000; ++i) {
         asm volatile("NOP");
     }
+    return 0xeeee;
 }
 
 MEMENTOS_MAIN_ATTRIBUTES
 int main (void) {
     WDTCTL = WDTPW + WDTHOLD; // stop WDT
     for (x = 0; x < 10; ++x) {
-        foo();
+        (void)foo();
     }
     return 99;
 }
