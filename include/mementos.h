f90c8d48e49efbd18cc2b9c7d2dab81641baaa5a
Ben Ransford <ben@ransford.org>
2014-02-06 10:02:40 -0800

    expunge stupid mementos.h.tmpl (just use .h)

--- include/mementos.h
+++ include/mementos.h
@@ -1,33 +1,4 @@
-/*
-Copyright (c) 2011, University of Massachusetts Amherst
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are
-met:
-
- 1. Redistributions of source code must retain the above copyright
-    notice, this list of conditions and the following disclaimer.
- 2. Redistributions in binary form must reproduce the above copyright
-    notice, this list of conditions and the following disclaimer in the
-    documentation and/or other materials provided with the distribution.
- 3. Neither the name of the University of Massachusetts Amherst nor the
-    names of its contributors may be used to endorse or promote products
-    derived from this software without specific prior written
-    permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+/* See license.txt for licensing information. */
 
 #ifndef __NEWMEM_H__
 #define __NEWMEM_H__
@@ -197,7 +168,7 @@ extern volatile unsigned int VOLTAGE_CHECK asm("0x01C0");
 
 #ifdef MEMENTOS_TIMER
 void Timer_A (void) __attribute__((interrupt(12))); // 0xFFEC for F1611 XXX
-//==TIMER_INTERVAL==
+#define TIMER_INTERVAL 20000
 #endif
 
 #define MEMREF_UINT(x) (*((unsigned int*)(x)))
