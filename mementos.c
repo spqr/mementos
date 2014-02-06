a6292dbba854ce374b77991583ec33a0cc471410
Ben Ransford <ben@ransford.org>
2014-02-06 12:23:02 -0800

    fix up offsets in __mementos_restore

--- mementos.c
+++ mementos.c
@@ -39,7 +39,7 @@ void __mementos_restore (unsigned long b) {
         asm volatile("SUB %0, %1" :"=m"(i) :"m"(j));
         asm volatile("DECD.W %0" ::"m"(j));
 
-        // k = baseaddr; k += 30; k += tmpsize; k -= i;
+        // k = baseaddr + BUNDLE_SIZE_REGISTERS + tmpsize - i;
         asm volatile("MOV %1, %0" :"=m"(k) :"m"(baseaddr));
         asm volatile("ADD #" xstr(BUNDLE_SIZE_REGISTERS) ", %0" ::"m"(k));
         asm volatile("ADD %1, %0" :"=m"(k) :"m"(tmpsize));
@@ -64,19 +64,23 @@ void __mementos_restore (unsigned long b) {
      *         memory[baseaddr + stacksize + regfilesize + headersize + i]
      * }
      */
+
+    // grab the size of the size of the globals we'll have to restore
     asm volatile("MOV %0, R7" ::"m"(tmpsize));  // R7(stacksize) = tmpsize
     asm volatile("MOV %0, R6" ::"m"(baseaddr)); // R6(baseaddr)  = baseaddr
     asm volatile("MOV @R6, R8\n\t"               // R8(globalsize) =
                  "AND #255, R8");                //   MEMREF(baseaddr) & 0x00FF
-    asm volatile("CLR.W R9");                    // R9(i) = 0 // induction var
 
+    asm volatile("CLR.W R9");                    // R9(i) = 0 // induction var
     asm volatile("rdloop:");                     // will jump back up here
-    asm volatile("CMP R8, R9\n\t"                // if (i(R9) >= globalsize(R8))
+    asm volatile("CMP R8, R9\n\t"                // if (i >= globalsize)
                  "JC afterrd");                  //   <stop looping>
-    asm volatile("MOV R6, R10\n\t"               // R10 = baseaddr(R6)
-                 "ADD #" xstr(STACK_OFFSET) ", R10\n\t"              //   + 32 // skip regs, header,
-                 "ADD R7, R10\n\t"               //   + stacksize(R7) // stack,
-                 "ADD R9, R10");                 //   + i(R9)
+
+    // copy one word at a time from checkpoint to data segment
+    asm volatile("MOV R6, R10\n\t"               // R10 = baseaddr
+                 "ADD #" xstr(STACK_OFFSET) ", R10\n\t" // + stack_offset
+                 "ADD R7, R10\n\t"               //        + stacksize
+                 "ADD R9, R10");                 //        + i
     asm volatile("MOV @R10, "                    // MEMREF(STARTOFDATA+i(R9)) =
             xstr(STARTOFDATA) "(R9)");           //        MEMREF(R10)
     asm volatile("INCD R9");                     // i += 2
@@ -96,41 +100,41 @@ void __mementos_restore (unsigned long b) {
     /* set the SP first, so we can PUSH stuff; we'll reset it later */
     asm volatile ("MOV 4(R6), R1");
     /* now push the saved register values onto the stack (R6=baseaddr) */
-    asm volatile ("PUSH 30(R6)\n\t"  // R15 is at 28(SP)
-                  "PUSH 28(R6)\n\t"  // R14 is at 26(SP)
-                  "PUSH 26(R6)\n\t"  // R13 is at 24(SP)
-                  "PUSH 24(R6)\n\t"  // R12 is at 22(SP)
-                  "PUSH 22(R6)\n\t"  // R11 is at 20(SP)
-                  "PUSH 20(R6)\n\t"  // R10 is at 18(SP)
-                  "PUSH 18(R6)\n\t"  // R9 is at 16(SP)
-                  "PUSH 16(R6)\n\t"  // R8 is at 14(SP)
-                  "PUSH 14(R6)\n\t"  // R7 is at 12(SP)
-                  "PUSH 12(R6)\n\t"  // R6 is at 10(SP)
-                  "PUSH 10(R6)\n\t"  // R5 is at 8(SP)
-                  "PUSH 8(R6)\n\t"   // R4 is at 6(SP)
+    asm volatile ("PUSH 60(R6)\n\t"  // R15
+                  "PUSH 56(R6)\n\t"  // R14
+                  "PUSH 52(R6)\n\t"  // R13
+                  "PUSH 48(R6)\n\t"  // R12
+                  "PUSH 44(R6)\n\t"  // R11
+                  "PUSH 40(R6)\n\t"  // R10
+                  "PUSH 36(R6)\n\t"  // R9
+                  "PUSH 32(R6)\n\t"  // R8
+                  "PUSH 28(R6)\n\t"  // R7
+                  "PUSH 24(R6)\n\t"  // R6
+                  "PUSH 20(R6)\n\t"  // R5
+                  "PUSH 16(R6)\n\t"  // R4
                   // skip R3 (CG)
-                  "PUSH 6(R6)\n\t"   // R2 is at 4(SP)
-                  "PUSH 4(R6)\n\t"   // R1 is at 2(SP)
-                  "PUSH 2(R6)");     // R0 is at 0(SP)
+                  "PUSH 12(R6)\n\t"  // R2
+                  "PUSH 8(R6)\n\t"   // R1
+                  "PUSH 4(R6)");     // R0
 
     /* finally, overwrite the register file with the saved values from the
      * stack!  don't do the PC yet! */
-    asm volatile ("MOV 28(R1), R15\n\t"
-                  "MOV 26(R1), R14\n\t"
-                  "MOV 24(R1), R13\n\t"
-                  "MOV 22(R1), R12\n\t"
-                  "MOV 20(R1), R11\n\t"
-                  "MOV 18(R1), R10\n\t"
-                  "MOV 16(R1), R9\n\t"
-                  "MOV 14(R1), R8\n\t"
-                  "MOV 12(R1), R7\n\t"
-                  "MOV 10(R1), R6\n\t"
-                  "MOV  8(R1), R5\n\t"
-                  "MOV  6(R1), R4\n\t"
-                  "MOV  4(R1), R2\n\t"
+    asm volatile ("MOV 56(R1), R15\n\t"
+                  "MOV 52(R1), R14\n\t"
+                  "MOV 48(R1), R13\n\t"
+                  "MOV 44(R1), R12\n\t"
+                  "MOV 40(R1), R11\n\t"
+                  "MOV 36(R1), R10\n\t"
+                  "MOV 32(R1), R9\n\t"
+                  "MOV 28(R1), R8\n\t"
+                  "MOV 24(R1), R7\n\t"
+                  "MOV 20(R1), R6\n\t"
+                  "MOV 16(R1), R5\n\t"
+                  "MOV 12(R1), R4\n\t"
+                  "MOV  8(R1), R2\n\t"
                   /* now set the stack pointer -- don't use any more locals from
                    * here to when we restore the PC */
-                  "MOV  2(R1), R1");
+                  "MOV  4(R1), R1");
 
     j = MEMREF_UINT(baseaddr + BUNDLE_SIZE_HEADER);
     asm volatile ("MOV %0, R0" ::"m"(j)); // implicit jump ... restored!
